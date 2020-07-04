dbac7944a5ec700c567dcd72e1bb9ca5afc6a804
Gabriel-developer-01 <gabriel.mendes.souza1@hotmail.com>
2020-07-03 21:45:14 -0300

    ideia extra - Classe main do projeto que uni o Program1 e o Program2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,56 +1,34 @@
 package application;
 
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
-import model.dao.DaoFactory;
-import model.dao.SellerDao;
-import model.entities.Department;
-import model.entities.Seller;
+import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class Program {
-
+/*ATENÇÃO: ESSA CLASSE FOI CRIADA COMO DESAFIO PARA MIM EXERCITAR,FIZ UM MENU PARA JUNTAR O PROGRAM1 E O PROGRAM2*/
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-	
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== TESTE 1: seller findById ====");
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
 
-		System.out.println("\n==== TESTE 2: seller findByDepartment ====");
-		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
-		for(Seller obj : list ) {
-			System.out.println(obj);
-		}	
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("\n==== TESTE 3: seller findAll ====");
-		list = sellerDao.findAll();
-		for(Seller obj : list ) {
-			System.out.println(obj);
+		//chamada no meu menu para escolher Department ou Seller
+		ShowView.showGetInfoFromMenu();
+		System.out.println("choose an option: ");
+		int menu = sc.nextInt();
+
+		switch (menu) {
+
+		case 1:
+			//caso seja Department eu chamo o método que trás o menu com as opções
+			DepartmentService.Department();
+			break;
+
+		case 2:
+			//caso seja Seller eu chamo o método que trás o menu com as opções
+			SellerService.Seller();
+			break;
 		}
-		
-		System.out.println("\n==== TESTE 4: seller Insert ====");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0,department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
-		
-		System.out.println("\n==== TESTE 5: seller update ====");
-		seller = sellerDao.findById(1);
-		seller.setName("Martha Waine");
-		sellerDao.update(seller);
-		System.out.println("Update completed");
-		
-		System.out.println("\n==== TESTE 6: seller delet ====");
-		System.out.print("Enter id for delete test: ");
-		int id = sc.nextInt();
-		sellerDao.deleteById(id);
-		System.out.println("Delete completed");
-		
+
 		sc.close();
 	}
 }
